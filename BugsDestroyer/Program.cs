4f9886300158ef02f5ae1b21cf7c724b0aa800da
aymeric.sgnth <aymeric.sgnth@eduge.ch>
2022-09-01 09:35:08 +0200

    Add header comments

--- BugsDestroyer/Program.cs
+++ BugsDestroyer/Program.cs
@@ -2,6 +2,13 @@
 
 namespace BugsDestroyer
 {
+    /*
+     * Thibaud Hegelbach, Aymeric Siegenthaler, Yoann Meier, Alexandre Babich
+     * Program.cs
+     * Main file of program
+     * Version 2.0
+     * Original game has been taken here : https://github.com/AlecInfo/BugsDestroyer
+     */
     public static class Program
     {
         [STAThread]
